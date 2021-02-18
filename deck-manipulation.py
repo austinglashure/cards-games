9d329fef2952d76efd459e3674078018c8a854b6
Austin LaShure <austinglashure@gmail.com>
2021-02-17 20:20:58 -0500

    added shuffle and deal functionality

--- deck-manipulation.py
+++ deck-manipulation.py
@@ -1,5 +1,6 @@
 import random
 
+
 def create_deck():
 	
 	suits = "SDCH"
@@ -13,5 +14,31 @@ def create_deck():
 	
 	return deck
 
-deck = create_deck()
 
+def shuffle_deck(deck):
+	
+	random.shuffle(deck)
+	random.shuffle(deck)
+	random.shuffle(deck)
+
+
+def deal_deck(deck, player):
+	if len(deck) == 1:
+		card = deck.pop(0)
+		deck = create_deck()
+		shuffle_deck(deck)
+		player.append(card)
+		return deck
+	
+	else:
+		card = deck.pop(0)
+		player.append(card)
+		return deck
+	
+
+deck = create_deck()
+shuffle_deck(deck)
+player1 = []
+print(player1)
+deal_deck(deck, player1)
+print(player1)
