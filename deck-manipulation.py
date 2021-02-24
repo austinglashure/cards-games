521b67b5c4830e6d00df6d27d004a1152990e36d
Austin LaShure <austinglashure@gmail.com>
2021-02-23 22:01:19 -0500

    OOP'ified the code

--- deck-manipulation.py
+++ deck-manipulation.py
@@ -1,44 +1,28 @@
 import random
 
 
-def create_deck():
-	
-	suits = "SDCH"
-	values = "23456789TJQKA"
-	deck = []
-	
-	for suit in suits:
-		for value in values:
-			card = (value, suit)
-			deck.append(card)
-	
-	return deck
+class Deck:
+
+	def __init__(self, no_of_decks):
+
+		suits = "SDCH"  # Spades, Diamonds, Clubs, Hearts
+		faces = "23456789TJQKA"
+		self.shoe = []  # a fancy name for a deck of multiple decks
+		for dck in range(no_of_decks):
+			self.deck = []
+			for suit in suits:
+				for face in faces:
+					self.card = face + suit
+					self.deck.append(self.card)
+			random.shuffle(self.deck)
+			self.shoe += self.deck
+
+	def deal_card(self):
+		return self.shoe.pop(0)
+
+	def show_deck(self):
+		for card in self.shoe:
+			print(card)
 
 
-def shuffle_deck(deck):
-	
-	random.shuffle(deck)
-	random.shuffle(deck)
-	random.shuffle(deck)
-
-
-def deal_deck(deck, player):
-	if len(deck) == 1:
-		card = deck.pop(0)
-		deck = create_deck()
-		shuffle_deck(deck)
-		player.append(card)
-		return deck
-	
-	else:
-		card = deck.pop(0)
-		player.append(card)
-		return deck
-	
-
-deck = create_deck()
-shuffle_deck(deck)
-player1 = []
-print(player1)
-deal_deck(deck, player1)
-print(player1)
+deck = Deck(1)
